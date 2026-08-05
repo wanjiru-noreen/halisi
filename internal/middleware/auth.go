9aa9e54d0bdae0e601cc85280d033f1a4468ecdf
nowanjiru <mynahnoreen@gmail.com>
2026-08-05 23:38:20 +0300

    feat(auth): update authentication middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,16 @@
 package middleware
+
+import "net/http"
+
+func RequireAuth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		_, ok := GetCurrentUserID(r)
+		if !ok {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
