623dfa1b4867f10819a59dac7de0592060d60ee5
nowanjiru <mynahnoreen@gmail.com>
2026-08-05 23:38:39 +0300

    feat(models): update shop model

--- internal/models/shop.go
+++ internal/models/shop.go
@@ -1,9 +1,11 @@
 package models
 
 type Shop struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Location string `json:"location"`
-	Phone    string `json:"phone"`
-	OwnerID  *int   `json:"owner_id"`
+	ID        int     `json:"id"`
+	Name      string  `json:"name"`
+	Location  string  `json:"location"`
+	Phone     string  `json:"phone"`
+	Price6kg  float64 `json:"price_6kg"`
+	Price13kg float64 `json:"price_13kg"`
+	OwnerID   *int    `json:"owner_id"`
 }
