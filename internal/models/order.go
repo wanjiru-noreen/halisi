394be8a0565361194c33c9f5dc63625dd050643d
wanjiru-noreen <mynahnoreen@gmail.com>
2026-08-07 11:26:19 +0300

    feat(order): improve order model and repository

--- internal/models/order.go
+++ internal/models/order.go
@@ -1,11 +1,13 @@
 package models
 
 type Order struct {
-	ID           int
-	UserID       int
-	ShopID       int
-	CylinderSize string
-	Quantity     int
-	TotalPrice   float64
-	Status       string
+	ID              int
+	UserID          int
+	ShopID          int
+	CylinderSize    string
+	Quantity        int
+	TotalPrice      float64
+	Status          string
+	DeliveryAddress string
+	ShopName        string
 }
